b89dc4e639c137fe7518f2c5b48902b9936ad636
Erik kvanli <0erikkvanli@gmail.com>
2013-02-23 13:54:54 +0100

    H

---
+++ auction/src/Operation.java
@@ -0,0 +1,10 @@
+public class Operation {
+	public final double num1;
+	public final double num2;
+	public final char oper;
+	public Operation(double n1, double n2, char op){
+		this.num1=n1;
+		this.num2=n2;
+		this.oper=op;
+	}
+}
